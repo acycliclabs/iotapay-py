8d95c25f5bd1ca677a44dfa92f79f0b6b64d0269
Kumar Anirudha <anirudhastark@yahoo.com>
2019-03-05 16:12:09 +0530

    payment api and get balance api working.

--- iotapay/iotapay.py
+++ iotapay/iotapay.py
@@ -2,15 +2,10 @@
 from iota import Address, ProposedTransaction, Tag, Transaction, Iota, TryteString, json
 import time, json
 
-
-# Generate a random seed.
-tag = Tag('ACYCLICIOTAPAYLIOTA99999999') # Tag for in the message.
-
 class Iotapay:
     def __init__(self, provider, seed, address=None):
         self.seed = seed
         self.provider = provider
-        # self.api = Iota(self.provider)
         self.api = Iota(self.provider, self.seed)
 
     def pay(self, data):
@@ -18,28 +13,30 @@ class Iotapay:
             json_data = data['json_data']
             json_string = json.dumps(json_data)
             trytes = TryteString.from_string(json_string)
-            print('sending transfer ...')
             # Send Transfer
             sent_transfer = self.api.send_transfer(
                     depth = 3,
-                    transfers = [ProposedTransaction(address =  Address(Address(data['to_address'],)),
-                      value = data['amount'],
-                      tag = tag,
-                      message = trytes,),],)
-            print('sent_transfer', sent_transfer)
+                    transfers = [
+                            ProposedTransaction(
+                                address =  Address(data['to_address']),
+                                value = data['amount'],
+                                tag = Tag(data['tag']),
+                                message = trytes,
+                            ),
+                        ]
+                    )
             bo = sent_transfer['bundle']
-            print('bundle', bo)
             return {
                     'status': 200,
                     'transaction_hash': bo.as_json_compatible()[0]['hash_'],
                     'message': 'Successfully Sent!'
                 }
         except Exception as pe:
-            print('pe:', pe)
+            # print('pe:', pe)
             return {
                     'status': 400,
                     'error': '',
-                    'balance': str(pe.context['total_balance']),
+                    # 'balance': str(pe.context['total_balance']),
                     'message': str(pe).split('(')[0]
                 }
 
@@ -47,17 +44,21 @@ class Iotapay:
         try:
             if 'address' in data:
                 balance_result = self.api.get_balances([data['address']])
-                print('balance_result:', balance_result)
-                # {'balances': [0], 'milestone': None, 'duration': 1, 'milestoneIndex': 870196, 'references': ['NRZPIXFHBKGDEZCVEBHZQTJFDWKBPZSHZXEMCEOEDQYXOSYQKMXYYTHHUUJAKWX9YWNEIJPS9PFXZ9999']}
                 balance = balance_result['balances'][0]
-                # print('balance:', balance)
-                return {
-                        'status': 200,
-                        'balance': balance,
-                        'message': 'Successfully Retrieved!'
-                    }
+            else:
+                gna_result = self.api.get_new_addresses(index=0, count=50) # generate 50 addresses.
+                addresses = gna_result['addresses']
+                balance_result = self.api.get_balances(addresses)
+                balance = 0
+                for i in range(len(balance_result['balances'])):
+                    balance = balance + balance_result['balances'][i]
+            return {
+                    'status': 200,
+                    'balance': balance,
+                    'message': 'Successfully Retrieved!'
+                }
         except Exception as pe:
-            print('pe:', pe)
+            # print('pe:', pe)
             return {
                     'status': 400,
                     'error': '',
@@ -67,4 +68,8 @@ class Iotapay:
 
     def collect(self, data):
         print('send request to collect payment.')
+        return;
+
+    def generate_invoice(self, data):
+        print('generate invoice.')
         return;
--- iotapay/test.py
+++ iotapay/test.py
@@ -2,7 +2,7 @@
 from iotapay import Iotapay
 
 # Generate a random seed
-seed = 'KGWH9PTURNDTSHSQVNLOSFMSTQLHRTTQGDTFJHEKRNPVDRQGHQENANLSXXCXGICCVWBQHMHWPYZSHJYTC'
+seed = ''
 
 provider_url = 'https://potato.iotasalad.org:14265'
 
@@ -11,23 +11,24 @@ iotapay = Iotapay(provider_url, seed)
 # iotapay = Iotapay(seed, address)
 
 # Calling Pay function
-# pay_request_data = {
-#         'json_data': {
-#             'version': '0.1',
-#         'tag': 'ACYCLICIOTAPAYLIOTA99999999'
-#         },
-#         'to_address': 'JRNRWSOMSVMCGXOWBAZZNM9IJZFFQKQUMWFPWMVAICNFLAVMJYIPLOQOGPCHLQX9EJFIAIUQXJIMKDVPWGDTXMDKT9',
-#         # SEED: 'JVKFSJXZOUWXZESUWANGHK9WPNESHWDTWL9QUXYQCYIFZHMWLJKPPBXPPFZLLXMDEHNMHF9KVXUGWURGG',
-#         'amount': 2
-#     }
-#
+pay_request_data = {
+        'json_data': {
+            'version': '0.1',
+        },
+        'tag': 'ACYCLICIOTAPAYLIOTA99999999',
+        'to_address': '',
+        'amount': 9
+    }
+
 # paid_result = iotapay.pay(pay_request_data)
 # print('paid_result:', paid_result)
 
 # Get Balance API
-get_balance_data = {
-        'address': 'JRNRWSOMSVMCGXOWBAZZNM9IJZFFQKQUMWFPWMVAICNFLAVMJYIPLOQOGPCHLQX9EJFIAIUQXJIMKDVPWGDTXMDKT9'
-    }
+# Case 1: Get balance for particular address.
+# get_balance_data = { 'address': '' }
 
-balance_result = iotapay.get_balance(get_balance_data)
-print('balance_result:', balance_result)
+# Case 2: Get balance for n (currently 50) addresses connected to the seed.
+get_balance_data = {}
+
+# balance_result = iotapay.get_balance({})
+# print('balance_result:', balance_result)
